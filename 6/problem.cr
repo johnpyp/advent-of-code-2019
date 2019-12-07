0b8ef4e4f3017ee19debd13b185cbf57eba92474
johnpyp <johnpaulpenaloza@gmail.com>
2019-12-07 14:17:36 -0500

    Cleaned up day 6

--- 6/problem.cr
+++ 6/problem.cr
@@ -15,29 +15,20 @@ def collect_path(map, key, end_point = "COM")
   path
 end
 
-def input_to_map(input)
-  input.each_with_object({} of String => String) { |x, map|
-    a, b = x.split(')')
-    map[b] = a
-  }
-end
-
-def run(input)
-  map = input_to_map(input)
-  map.reduce(0) { |acc, x|
-    k, v = x
-    acc + collect_path(map, k).size
-  }
-end
-
-def run2(input)
-  map = input_to_map(input)
-  san_path = collect_path map, "SAN"
-  you_path = collect_path map, "YOU"
-  shared = (san_path & you_path)[0]
-  san_path.index(shared).as(Int32) + you_path.index(shared).as(Int32)
-end
-
 input = File.read_lines("input.txt")
-puts run input
-puts run2 input
+map = input.each_with_object({} of String => String) { |x, map|
+  a, b = x.split(')')
+  map[b] = a
+}
+# part 1
+puts map.reduce(0) { |acc, x|
+  k, v = x
+  acc + collect_path(map, k).size
+}
+# part 2
+san_path = collect_path map, "SAN"
+you_path = collect_path map, "YOU"
+
+shared = (san_path & you_path)[0]
+
+puts san_path.index(shared).as(Int32) + you_path.index(shared).as(Int32)
